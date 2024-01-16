4cf6e5fa42a5835aa38bb6ea34f4e79b0d839aaf
yuna <155256032+yuna1-u@users.noreply.github.com>
2024-01-16 11:44:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Factory factory = new Factory("milk", 1_000_000, "Rabia", 500);
-        Factory factory1 = new Factory("Phone", 100_000_000, "Sami", 5000);
-        Factory factory2 = new Factory("Car",800_000_000,"Qwerty",7000000);
+        Factory factory = new Factory("milk", 1_000_000, "Rabia");
+        Factory factory1 = new Factory("Phone", 100_000_000, "Sami");
+        Factory factory2 = new Factory("Car",800_000_000,"Qwerty");
         Factory factory3 = new Factory();
         Factory factory4 = new Factory();
 
